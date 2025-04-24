cdde779c1007f34efbb1245a53660a6b58fc6d91
Kelchain <MatthewJKeller@gmail.com>
2025-04-23 22:17:59 -0600

    Checking and Calculating Balances

--- test/AMM.js
+++ test/AMM.js
@@ -44,18 +44,67 @@ describe('AMM', () => {
       expect(amm.address).to.not.equal(0x0)
     })
 
-    it('returns token1 address', async () => {
+    it('trackstoken1 address', async () => {
       expect(await amm.token1()).to.equal(token1.address)
     })
 
-    it('returns token2 address', async () => {
+    it('tracks token2 address', async () => {
       expect(await amm.token2()).to.equal(token2.address)
     })
   
   describe('Swapping tokens', () => {
+    let amount, transaction, result
     
-    it('faciltates swaps', async () => {
-      expect(amm.address).to.not.equal(0x0)
+    it('facilitates swaps', async () => {
+
+      amount = tokens(100000)
+      
+      transaction = await token1.connect(deployer).approve(amm.address, amount)
+      await transaction.wait()
+
+      transaction = await token2.connect(deployer).approve(amm.address, amount)
+      await transaction.wait()
+
+      
+
+      
+      transaction = await amm.connect(deployer).addLiquidity(amount, amount)
+      await transaction.wait()
+
+      expect(await token1.balanceOf(amm.address)).to.equal(amount)
+      expect(await token2.balanceOf(amm.address)).to.equal(amount)
+
+      expect(await amm.token1Balance()).to.equal(amount)
+      expect(await amm.token2Balance()).to.equal(amount)
+
+      expect(await amm.shares(deployer.address)).to.equal(tokens(100))
+
+      expect(await amm.totalShares()).to.equal(tokens(100))
+
+
+
+      amount = tokens(50000)
+      
+      transaction = await token1.connect(liquidityProvider).approve(amm.address, amount)
+      await transaction.wait()
+
+      transaction = await token2.connect(liquidityProvider).approve(amm.address, amount)
+      await transaction.wait()
+
+      let token2Deposit = await amm.calculateToken2Deposit(amount)
+
+
+      transaction = await amm.connect(liquidityProvider).addLiquidity(amount, token2Deposit)
+      await transaction.wait()
+
+      expect(await amm.shares(liquidityProvider.address)).to.equal(tokens(50))
+
+      expect(await amm.shares(deployer.address)).to.equal(tokens(100))
+
+      expect(await amm.totalShares()).to.equal(tokens(150))
+
+
+
     })
 
     //it('returns token1 address', async () => {

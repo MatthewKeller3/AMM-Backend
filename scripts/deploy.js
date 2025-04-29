87bd8bd0c79a686368f1a320f72cc375944aa6b8
Kelchain <MatthewJKeller@gmail.com>
2025-04-29 16:37:25 -0600

    AMM Backend Completegit add .!

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,17 +9,16 @@ const hre = require("hardhat");
 async function main() {
   const Token = await hre.ethers.getContractFactory('Token')
 
-  // Deploy Token 1
+
   let dapp = await Token.deploy('Dapp Token', 'DAPP', '1000000') // 1 million tokens
   await dapp.deployed()
   console.log(`Dapp Token deployed to: ${dapp.address}\n`)
 
-  // Deploy Token 2
+
   const usd = await Token.deploy('USD Token', 'USD', '1000000') // 1 million tokens
   await usd.deployed()
   console.log(`USD Token deployed to: ${usd.address}\n`)
 
-  // Deploy AMM
   const AMM = await hre.ethers.getContractFactory('AMM')
   const amm = await AMM.deploy(dapp.address, usd.address)
 
@@ -27,8 +26,6 @@ async function main() {
 
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
